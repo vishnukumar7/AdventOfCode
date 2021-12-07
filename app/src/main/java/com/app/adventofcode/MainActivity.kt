f9d77a91b9a0b2f2dc492c0df7c04e51f812a2a9
Vishnu Kumar G R <grvishnukumar@hitasoft.com>
2021-12-07 15:35:30 +0530

    finish year 2020 upto day 6

--- app/src/main/java/com/app/adventofcode/MainActivity.kt
+++ app/src/main/java/com/app/adventofcode/MainActivity.kt
@@ -1,21 +1,10 @@
 package com.app.adventofcode
 
-import android.content.Intent
-import android.net.Uri
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
-import android.provider.Settings
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.app.adventofcode.model.AdventCode
-import com.app.adventofcode.yearTwenty.BinaryBoardingDayFive
-import com.app.adventofcode.yearTwenty.CustomDaySix
-import com.app.adventofcode.yearTwenty.HandheldHaltingDayEight
-import com.app.adventofcode.yearTwenty.PassportProcessingDayFour
 import java.io.BufferedReader
-import java.io.File
-import java.io.FileInputStream
 import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
@@ -27,24 +16,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),100)
-        }
         listItemSentence= ArrayList()
         listItemMultiList= ArrayList()
         listItemAdventCode= ArrayList()
-        listItem=getValues()
-        val binaryBoardingDayFive= HandheldHaltingDayEight(listItemAdventCode)
-        Log.d(TAG, "onCreate: binary part one ${binaryBoardingDayFive.partOne()}")
-        Log.d(TAG, "onCreate: binary part two ${binaryBoardingDayFive.partTwo()}")
+        listItem=getAssetsFile("")
 
     }
 
-    fun getValues() : ArrayList<String> {
-        val file = File(Environment.getExternalStorageDirectory().absolutePath,"Code.txt")
-        val fileInputStream = FileInputStream(file)
-        val inputStreamReader = InputStreamReader(fileInputStream)
-        val bufferedReader = BufferedReader(inputStreamReader)
+    fun getAssetsFile(fileName: String) : ArrayList<String> {
+       val bufferedReader= BufferedReader(InputStreamReader(assets.open(fileName)))
         val arrList = ArrayList<String>()
         var line: String?
         var wordCount = 0
@@ -83,27 +63,5 @@ class MainActivity : AppCompatActivity() {
             listItemSentence.add(sente)
         Log.d(TAG, "getValues: para $count")
         return arrList
-    }
-
-    private fun getImageFromExternal() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            if (Environment.isExternalStorageManager()) {
-                //println(getValues())
-            } else {
-                val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
-                val uri: Uri = Uri.fromParts("package", packageName, null)
-                intent.data = uri
-                startActivity(intent)
-            }
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        getImageFromExternal()
     }
 }
